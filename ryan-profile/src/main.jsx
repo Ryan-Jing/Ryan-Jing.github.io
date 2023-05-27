0b47480e1b456ead7a5198ed983173c83b9c78f7
Ryan-Jing <ryanjing23@gmail.com>
2023-05-27 16:09:16 -0400

    Added Navbar and background images

--- ryan-profile/src/main.jsx
+++ ryan-profile/src/main.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App.jsx'
-import './index.css'
+//import About from './About.jsx';
+import './index.css';
+
+/* Check this for future multiple page
+const Main = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/About" component={About} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
+*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
