fab2670f1e6a0c57f2058442fb6c4e035bad351e
Ryan-Jing <ryanjing23@gmail.com>
2023-08-13 18:14:01 -0400

    Fixed lots of issues

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,6 +15,30 @@ function About(){
     header.classList.toggle('scrolling_active', windowPosition )
   })
 
+  useEffect(() => {
+    const header = document.getElementById("fadein1");
+    const duration = 1000; // Animation duration in milliseconds
+    const start = performance.now();
+    
+    const easeInOutQuad = t => t<.5 ? 2*t*t : -1+(4-2*t)*t;
+    
+    const fade = () => {
+      const elapsed = performance.now() - start;
+      const progress = Math.min(elapsed / duration, 1); // Ensure progress doesn't exceed 1
+      const easedProgress = easeInOutQuad(progress);
+      header.style.opacity = easedProgress;
+      
+      if (progress < 1) {
+        window.requestAnimationFrame(fade);
+      } else {
+        setIsTextVisible(true); // Start text fade-in animation after header animation completes
+      }
+    };
+    
+    fade();
+  }, []);
+
+
   return (
     <>
       <nav className = "navbar">
@@ -49,7 +73,20 @@ function About(){
       </div>
     </nav>
       
-    <h1>About Me Page</h1>
+    {/* <h1>About Me Page</h1> */}
+    <div class = "Profile_Header">
+        <p1 id = "fadein1">
+          Profile
+        </p1>
+
+        {/* FIGURE OUR FADING IN FOR IMAGE */}
+
+        <img className="Profile_Image " src="/ProfileImage.JPG" alt="Fading Image" />
+
+        <p2>
+          My name is Ryan Jing. I am a Biomedical Engineering undergraduate student at the University of Waterloo. I have a strong passion for learning new skills and creating meaningful projects. My hobbies include music, visual art, sports (especially winter sports), and cooking. I am actively seeking co-op opportunities to expand my knowledge and skills further, broaden my professional network, and establish myself as a promising engineer in the field.
+        </p2>
+    </div>
     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
   </>
